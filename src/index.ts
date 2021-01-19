749336be9a3781e2fb241784b0c995dd0b3359f3
codyarose <crose992@gmail.com>
2021-01-18 22:20:26 -0600

    feat: :sparkles: add session authentication

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import { MikroORM } from "@mikro-orm/core"
 import { buildSchema } from "type-graphql"
 import express from "express"
 import { ApolloServer } from "apollo-server-express"
+import redis from "redis"
+import session from "express-session"
+import connectRedis from "connect-redis"
 
 import { __prod__ } from "./contants"
 import mikroConfig from "./mikro-orm.config"
 import { HelloResolver } from "./resolvers/hello"
 import { PostResolver } from "./resolvers/post"
 import { UserResolver } from "./resolvers/user"
+import { MyContext } from "./types"
 
 const main = async () => {
 	const orm = await MikroORM.init(mikroConfig)
@@ -16,12 +20,31 @@ const main = async () => {
 
 	const app = express()
 
+	const RedisStore = connectRedis(session)
+	const redisClient = redis.createClient()
+
+	app.use(
+		session({
+			name: "qid",
+			store: new RedisStore({ client: redisClient, disableTouch: true }),
+			cookie: {
+				maxAge: 1000 * 60 * 60 * 24 * 365 * 10,
+				httpOnly: true,
+				sameSite: "lax",
+				secure: __prod__,
+			},
+			saveUninitialized: false,
+			secret: "alksddfi3ii3ifjk",
+			resave: false,
+		})
+	)
+
 	const apolloServer = new ApolloServer({
 		schema: await buildSchema({
 			resolvers: [HelloResolver, PostResolver, UserResolver],
 			validate: false,
 		}),
-		context: () => ({ em: orm.em }),
+		context: ({ req, res }): MyContext => ({ em: orm.em, req, res }),
 	})
 
 	apolloServer.applyMiddleware({ app })
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -5,6 +5,7 @@ import {
 	InputType,
 	Mutation,
 	ObjectType,
+	Query,
 	Resolver,
 } from "type-graphql"
 import argon2 from "argon2"
@@ -39,10 +40,20 @@ class UserResponse {
 
 @Resolver()
 export class UserResolver {
+	@Query(() => User, { nullable: true })
+	async me(@Ctx() { req, em }: MyContext) {
+		if (!req.session.userId) {
+			return null
+		}
+
+		const user = await em.findOne(User, { id: req.session.userId })
+		return user
+	}
+
 	@Mutation(() => UserResponse)
 	async register(
 		@Arg("options") options: UsernamePasswordInput,
-		@Ctx() { em }: MyContext
+		@Ctx() { em, req }: MyContext
 	): Promise<UserResponse> {
 		if (options.username.length <= 2) {
 			return {
@@ -85,15 +96,18 @@ export class UserResolver {
 					],
 				}
 			}
-			console.log("message", error.message)
 		}
+
+		// store user id session; set cookie on user, keep logged in
+		req.session.userId = user.id
+
 		return { user }
 	}
 
 	@Mutation(() => UserResponse)
 	async login(
 		@Arg("options") options: UsernamePasswordInput,
-		@Ctx() { em }: MyContext
+		@Ctx() { em, req }: MyContext
 	): Promise<UserResponse> {
 		const user = await em.findOne(User, { username: options.username })
 		if (!user) {
@@ -117,6 +131,8 @@ export class UserResolver {
 				],
 			}
 		}
+
+		req.session.userId = user.id
 
 		return { user }
 	}
